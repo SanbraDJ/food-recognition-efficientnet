e2640ac27e0eb72c040114d4bd5312d566e12fe9
SanbraDJ <76dmyt4fxq@privaterelay.appleid.com>
2025-11-09 15:50:59 -0500

    Fix syntax error in utils.py file

---
+++ utils.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+from some_module import Policy  # Assuming Policy is imported from some_module
+
+# Other code...
+
+# Line 170
+print(f"   Variable dtype: {policy.variable_dtype}") # Fixed the syntax error
